1a07d16b9bccf349438d2b99364390f651595a54
Seyma <63148309+seymagkts@users.noreply.github.com>
2023-03-02 21:11:38 +0300

    Update daily_vaccinations.py

--- daily_vaccinations.py
+++ daily_vaccinations.py
@@ -4,7 +4,7 @@ import pandas as pd
 data = pd.read_excel('country_vaccination_stats.xlsx')
 list = data["country"].unique()
 
-for i in list:
-    NaN_change = data[data["country"]==i]
+for name in list:
+    NaN_change = data[data["country"]==name]
     new = NaN_change["daily_vaccinations"].min()
     NaN_change.fillna(value=new,inplace=True)
